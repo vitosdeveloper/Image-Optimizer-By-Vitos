5d789ef97d4da597930cc08ef80b031e3b69d106
vitosnatios <vitosdeveloper@gmail.com>
2024-02-16 01:09:37 -0300

    Add project files.

---
+++ Image Optimizer By Vitos/Program.cs
@@ -0,0 +1,16 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageOptimizerByVitos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string imageFolder = @"C:/teste/image.jpg";
+            //var image = Image.Load(imageFolder);
+            //image.SaveAsWebp(@"C:/teste/imageWebp.webp");
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
